09f2986f84260e360e17f88a57eeaaff316a8157
Danube Phan <dphan@box.com>
2021-02-01 16:12:13 -0800

    mutations

--- server/resolvers.js
+++ server/resolvers.js
@@ -7,6 +7,13 @@ const Query = {
   jobs: () => db.jobs.list(),
 };
 
+const Mutation = {
+  createJob: (root, { input }) => {
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  },
+};
+
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
@@ -16,4 +23,4 @@ const Company = {
     db.jobs.list().filter((job) => job.companyId === company.id),
 };
 
-module.exports = { Query, Job, Company };
+module.exports = { Query, Mutation, Job, Company };
